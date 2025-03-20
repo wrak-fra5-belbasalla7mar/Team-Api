fd07f0282d4db2d93e6b645ed16dc086b5ef264a
Youssef Alm El Din <76973497+youssefalmeldin@users.noreply.github.com>
2025-03-20 21:15:39 +0200

    Delete src/main/java/com/Fawry/Team_Management_service/service/UserService.java

--- src/main/java/com/Fawry/Team_Management_service/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.Fawry.Team_Management_service.service;
-
-import com.Fawry.Team_Management_service.dto.UserDto;
-
-import java.util.Optional;
-
-public interface UserService {
-    public Optional<UserDto> getUserById(Long userId) ;
-}
